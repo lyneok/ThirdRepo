2affbe790e99684f886371efc369701b1f036cb2
lyneok <67384872+lyneok@users.noreply.github.com>
2020-07-03 11:54:00 -0400

    Update hangman.py
    split with space

--- hangman.py
+++ hangman.py
@@ -55,7 +55,7 @@ HANGMAN_PICS = [
 assert len(HANGMAN_PICS) == 7, 'must have 7 noose pics'
 
 
-WORDS = 'snake zombie sphinx vaporize unknown rhythm xylophone strength oxygen luxury jogging awkward subway equip buffalo jinx pajamas wristwatch zodiac queue wizard puppy kiwifruit fluffy jawbreaker'.split(',')
+WORDS = 'snake zombie sphinx vaporize unknown rhythm xylophone strength oxygen luxury jogging awkward subway equip buffalo jinx pajamas wristwatch zodiac queue wizard puppy kiwifruit fluffy jawbreaker'.split(' ')
 print(f"Words: {WORDS}")
 
 
