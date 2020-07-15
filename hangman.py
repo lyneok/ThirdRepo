fcbafc166645304ad3f52d90ee2471d5db99f3ab
lyneok <67384872+lyneok@users.noreply.github.com>
2020-07-15 11:37:57 -0400

    Update hangman.py
    counting number of different letters in a word

--- hangman.py
+++ hangman.py
@@ -53,6 +53,9 @@ def getRandomWord():
     import random
     idx = random.randint(0, len(WORDS)-1)
     return WORDS[idx]
+#Count the number of different letters in a word
+def count_letters_in_(word):
+    return len(set(word))
 
 #print(HANGMAN_PICS)
 #[print(pic) for pic in HANGMAN_PICS]
@@ -63,7 +66,9 @@ assert len(HANGMAN_PICS) == 7, 'must have 7 noose pics'
 WORDS = 'snake zombie sphinx vaporize unknown rhythm xylophone strength oxygen luxury jogging awkward subway equip buffalo jinx pajamas wristwatch zodiac queue wizard puppy kiwifruit fluffy jawbreaker'.split(' ')
 print(f"Words: {WORDS}")
 
-print(f"Random word: {getRandomWord()}") 
+print(f"Random word: {getRandomWord()}")
 
+
+assert count_letters_in_("aassacdee") == 5, "this word has 5 letters!" 
 
 
